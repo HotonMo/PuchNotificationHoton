d0588d8407cda64fe1351843d16fc62863ae151d
hotonMo1 <halharithi@waredat.sa>
2022-12-19 23:39:18 +0300

    Push Notifications

--- PuchNotificationHoton/PuchNotificationHotonApp.swift
+++ PuchNotificationHoton/PuchNotificationHotonApp.swift
@@ -1,17 +1,117 @@
 //
-//  PuchNotificationHotonApp.swift
-//  PuchNotificationHoton
+//  PuchNotificationApp.swift
+//  PuchNotification
 //
-//  Created by hoton on 25/05/1444 AH.
+//  Created by hoton on 24/05/1444 AH.
+//
 //
 
 import SwiftUI
+import FirebaseCore
+import UserNotifications
+import FirebaseMessaging
+
 
 @main
-struct PuchNotificationHotonApp: App {
+struct PuchNotificationApp : App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject,UIApplicationDelegate{
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+   // recive registration token
+        Messaging.messaging().delegate = self
+        
+    // show the permission diagoal
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        //  (registration token )
+        
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+    -> UIBackgroundFetchResult {
+        // BackgroundNotification " if user outside the app"
+        
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        print(userInfo)
+        
+        return UIBackgroundFetchResult.newData
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+    
+}
+
+extension AppDelegate: MessagingDelegate{
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        //   store token in firestore for seending notifations
+        //  send token to application server.
+        
+    }
+    
+}
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // Receive displayed notifications
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
+    -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        
+        
+        print(userInfo)
+        
+        
+        return [[.alert, .badge, .sound]]
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        
+        
+        print(userInfo)
     }
 }
