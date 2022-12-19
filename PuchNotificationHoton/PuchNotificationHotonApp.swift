d680a8509e1522c2d17df35fa4a4dd356709e45a
hotonMo1 <halharithi@waredat.sa>
2022-12-19 22:15:57 +0300

    Initial Commit

---
+++ PuchNotificationHoton/PuchNotificationHotonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuchNotificationHotonApp.swift
+//  PuchNotificationHoton
+//
+//  Created by hoton on 25/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PuchNotificationHotonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
